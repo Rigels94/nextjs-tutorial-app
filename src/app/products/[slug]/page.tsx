1094e750f277debe48f155c414cbf8ac51cbf34a
Hyeok Kyun Jo <whgurrbs@gmail.com>
2023-05-09 09:41:28 +0900

    feat: 5.8 SSG 렌더링

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,5 +1,5 @@
-import { getProduct, getProducts } from '@/service/products';
-import { notFound } from 'next/navigation';
+import { getProduct, getProducts } from "@/service/products";
+import { notFound } from "next/navigation";
 
 type Props = {
   params: {
@@ -7,27 +7,29 @@ type Props = {
   };
 };
 
-export function generateMetadata({ params }: Props) {
+export async function generateMetadata({ params }: Props) {
+  const product = await getProduct(params.slug);
+
   return {
-    title: `제품의 이름: ${params.slug}`,
+    title: `제품의 이름: ${product?.name ?? ""}`,
   };
 }
 
-export default function PantsPage({ params: { slug } }: Props) {
-  const product = getProduct(slug);
+export default async function PantsPage({ params: { slug } }: Props) {
+  const product = await getProduct(slug);
 
   if (!product) {
     notFound();
   }
 
   // 서버 파일에 있는 데이터중 해당 제품의 정보를 찾아서 그걸 보여줌
-  return <h1>{product} 제품 설명 페이지</h1>;
+  return <h1>{product.name} 제품 설명 페이지</h1>;
 }
 
-export function generateStaticParams() {
+export async function generateStaticParams() {
   // 모든 제품의 페이지들을 미리 만들어 둘 수 있게 해줄거임 (SSG)
-  const products = getProducts();
+  const products = await getProducts();
   return products.map((product) => ({
-    slug: product,
+    slug: product.id,
   }));
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,16 +1,16 @@
-import { getProducts } from '@/service/products';
-import Link from 'next/link';
+import { getProducts } from "@/service/products";
+import Link from "next/link";
 
-export default function ProductsPage() {
+export default async function ProductsPage() {
   // 서버 파일(데이터베이스)에 있는 제품의 리스트를 읽어와서, 그걸 보여줌
-  const products = getProducts();
+  const products = await getProducts();
   return (
     <>
       <h1>제품 소개 페이지!</h1>
       <ul>
-        {products.map((product, index) => (
-          <li key={index}>
-            <Link href={`/products/${product}`}>{product}</Link>
+        {products.map((product) => (
+          <li key={product.id}>
+            <Link href={`/products/${product.id}`}>{product.name}</Link>
           </li>
         ))}
       </ul>
