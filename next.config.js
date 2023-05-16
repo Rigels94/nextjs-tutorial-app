e6fa9d4a682bc2cd00feff67468165b0dc423a41
Hyeok Kyun Jo <whgurrbs@gmail.com>
2023-05-16 09:55:37 +0900

    feat: 6.9 redirects

--- next.config.js
+++ next.config.js
@@ -11,6 +11,25 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/products/deleted_forever",
+        destination: "/products",
+        permanent: true,
+      },
+      {
+        source: "/cars/:path*",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/products/deleted_temp",
+        destination: "/products",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
