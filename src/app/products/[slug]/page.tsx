be587333e080e095b4a3b00ac40f96f93d9eeb59
Hyeok Kyun Jo <whgurrbs@gmail.com>
2023-05-16 09:30:18 +0900

    fix: 6.6 상세페이지에 이미지추가

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { getProduct, getProducts } from "@/service/products";
+import Image from "next/image";
 import { notFound } from "next/navigation";
 
 type Props = {
@@ -23,7 +24,17 @@ export default async function PantsPage({ params: { slug } }: Props) {
   }
 
   // 서버 파일에 있는 데이터중 해당 제품의 정보를 찾아서 그걸 보여줌
-  return <h1>{product.name} 제품 설명 페이지</h1>;
+  return (
+    <>
+      <h1>{product.name} 제품 설명 페이지</h1>
+      <Image
+        src={`/images/${product.image}`}
+        alt="product image"
+        width={400}
+        height={400}
+      />
+    </>
+  );
 }
 
 export async function generateStaticParams() {
